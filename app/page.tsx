090ce0b7c914da20d6fb57ef0b2d47caecfb641d
Ayush Antiwal <ayukumar1010@gmail.com>
2024-09-23 00:57:17 +0530

    UI Update

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import content from "@/public/data/content.json";
 import Card from "./components/Card";
 import { Variants, motion } from "framer-motion";
 import github from "@/public/icons/github.svg";
+import { socials } from "@/public/data/socials";
 const cardVariants: Variants = {
   offscreen: {
     x: 50, // Starting position offscreen
@@ -29,28 +30,7 @@ const basic = {
   title: "Web Developer",
   summary: "I build user-friendly, efficient, and scalable web applications with modern technologies."
 };
-const socials = [
-  {
-    name: "GitHub",
-    link: "https://github.com/AyushAntwal",
-    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-6 w-6" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>`
-  },
-  {
-    name: "LinkedIn",
-    link: "https://www.linkedin.com/in/ayush-antiwal-dev/",
-    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" aria-hidden="true"><path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path></svg>`
-  },
-  {
-    name: "Email",
-    link: "mailto:ayushantiwal08@gmail.com",
-    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" aria-hidden="true"><path d="M17,21h3c1.65,0,3-1.35,3-3V9.92l-6,4.68V21z M19.5,3c-0.79,0-1.54,0.26-2.16,0.74L17,4.01v8.06l6-4.69V6.5	C23,4.57,21.43,3,19.5,3z M9,5.57v8.06l3,2.34l3-2.34V5.57l-3,2.34L9,5.57z M1,18c0,1.65,1.35,3,3,3h3v-6.4L1,9.92V18z M4.5,3	C2.57,3,1,4.57,1,6.5v0.88l6,4.69V4.01L6.66,3.74C6.04,3.26,5.29,3,4.5,3z"></path></svg>`
-  },
-  {
-    name: "Resume",
-    link: "./resume.pdf",
-    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" aria-hidden="true"><path d="M21.976,9h-5.976c-1.654,0-3-1.346-3-3V.024c-.161-.011-1.322-.024-1.485-.024H7C4.243,0,2,2.243,2,5v14c0,2.757,2.243,5,5,5h10c2.757,0,5-2.243,5-5V9.485c0-.163-.013-.324-.024-.485ZM9,4.999c1.105,0,2,.895,2,2s-.895,2-2,2-2-.895-2-2,.895-2,2-2Zm-2.744,6.392c.109-.143,1.113-1.392,2.77-1.392s2.659,1.25,2.769,1.392c.336,.438,.253,1.066-.186,1.402-.182,.139-.396,.206-.607,.206-.301,0-.598-.135-.795-.392,0,0-.497-.608-1.181-.608s-1.177,.602-1.182,.608c-.336,.439-.962,.523-1.402,.186-.438-.335-.521-.963-.186-1.402Zm10.744,9.609H7c-.553,0-1-.448-1-1s.447-1,1-1h10c.553,0,1,.448,1,1s-.447,1-1,1Zm0-4H7c-.553,0-1-.448-1-1s.447-1,1-1h10c.553,0,1,.448,1,1s-.447,1-1,1Zm-1-10c-.552,0-1-.448-1-1V.942c.527,.302,1.023,.666,1.465,1.107l3.484,3.486c.442,.441,.806,.937,1.108,1.464h-5.057Z"/></svg>`
-  }
-];
+
 export default function Home() {
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -93,7 +73,7 @@ export default function Home() {
                 <ul className=" gap-4 flex-col mt-8 hidden lg:flex">
                   {["about", "experience", "education", "projects", "skills"].map((item, i) => (
                     <li key={i}>
-                      <div id={"indicator-" + item} className="scroll-indicators group">
+                      <div id={"indicator-" + item} className="scroll-indicators relative">
                         <Link href={`#${item}`}>
                           {item}
                           <div className="i-border duration-500"></div>
@@ -110,9 +90,13 @@ export default function Home() {
                     target="_blank"
                     href={item.link}
                     download={item.name === "Resume" ? "Ayush_Antiwal_Resume.pdf" : undefined}
-                    className="group flex items-center gap-2 px-2 lg:px-4 py-2 bg-slate-900/75 rounded-lg text-teal-200 hover:bg-slate-900/50"
+                    className="group flex relative items-center gap-2 px-2 lg:px-4 py-2 bg-slate-900/75 rounded-lg text-teal-200 hover:bg-slate-900/50"
                   >
                     <span dangerouslySetInnerHTML={{ __html: item.icon }}></span>
+
+                    <div className="absolute -top-8 translate-y-5 right-0 left-0 w-fit -scale-50 opacity-0 transition-all duration-300 ease-in-out bg-black/20 text-teal-300 rounded-xl px-3 py-1 group-hover:translate-y-0 group-hover:scale-100 group-hover:opacity-100">
+                      {item.name}
+                    </div>
                   </a>
                 ))}
               </div>
@@ -182,7 +166,7 @@ export default function Home() {
                 ))}
               </ul>
             </section>
-            <section id="education" className="mb-16 section scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+            <section id="education" className="mb-16 section scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-28">
               <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">Education</h2>
               </div>
